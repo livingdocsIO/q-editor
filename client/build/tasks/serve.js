b399658edfa4d6e4323c1a6bc45feb73f19a065c
Ralph Meier <ralphmeier79@gmail.com>
2019-08-29 16:31:37 +0200

    fix: make port configurable in server.js

--- client/build/tasks/serve.js
+++ client/build/tasks/serve.js
@@ -4,52 +4,17 @@ var browserSync = require('browser-sync');
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9001
+
 gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9001,
+    port: require('../../../config').port,
     ui: {
-      port: 9999
+      port: require('../../../config').port + 1
     },
     server: {
       baseDir: ['.'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  }, done);
-});
-
-// this task utilizes the browsersync plugin
-// to create a dev server instance
-// at http://localhost:9001
-gulp.task('serve-bundle', ['bundle'], function(done) {
-  browserSync({
-    online: false,
-    open: false,
-    port: 9001,
-    server: {
-      baseDir: ['.'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  }, done);
-});
-
-// this task utilizes the browsersync plugin
-// to create a dev server instance
-// at http://localhost:9001
-gulp.task('serve-export', ['export'], function(done) {
-  browserSync({
-    online: false,
-    open: false,
-    port: 9001,
-    server: {
-      baseDir: ['./export'],
       middleware: function(req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
