187c4487c12a115b67c704c47f544afe7ede0437
Beni Buess <beni@livingdocs.io>
2019-12-11 14:46:00 +0100

    feat(config): load config and translations with authorization header

--- client/src/resources/QConfig.js
+++ client/src/resources/QConfig.js
@@ -1,9 +1,21 @@
+import { inject } from "aurelia-framework";
 import qEnv from "resources/qEnv.js";
+import User from "resources/User.js";
+import { HttpClient } from "aurelia-fetch-client";
 
+@inject(User, HttpClient)
 export default class QConfig {
-  constructor() {
-    this.configLoaded = qEnv.QServerBaseUrl.then(QServerBaseUrl => {
-      return fetch(`${QServerBaseUrl}/editor/config`);
+  constructor(user, httpClient) {
+    this.httpClient = httpClient;
+    this.configLoaded = new Promise(resolve => {
+      this.resolveConfigLoaded = resolve;
+    });
+    user.loaded.then(() => {
+      return qEnv.QServerBaseUrl.then(QServerBaseUrl => {
+        return this.httpClient.fetch(`${QServerBaseUrl}/editor/config`, {
+          credentials: 'include'
+        });
+      });
     })
       .then(response => {
         if (response.ok) {
@@ -13,6 +25,7 @@ export default class QConfig {
       })
       .then(config => {
         this.config = config;
+        this.resolveConfigLoaded();
       })
       .catch(e => {
         this.config = null;
