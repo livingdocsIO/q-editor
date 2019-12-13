98f73264d188e2a7edf0f485e84ac8e06e8d7d96
Beni Buess <beni@livingdocs.io>
2019-12-13 17:30:38 +0100

    fix(stats): do not show statistics if they cannot get loaded

--- client/src/resources/Statistics.js
+++ client/src/resources/Statistics.js
@@ -2,23 +2,19 @@ import qEnv from "resources/qEnv.js";
 
 export default class Statistics {
   async getNumberOfActiveItems(inLastDaysCount = undefined) {
-    try {
-      const QServerBaseUrl = await qEnv.QServerBaseUrl;
+    const QServerBaseUrl = await qEnv.QServerBaseUrl;
 
-      let url = `${QServerBaseUrl}/statistics/number-of-items`;
-      if (inLastDaysCount) {
-        url += `/${Date.now() - inLastDaysCount * 24 * 60 * 60 * 1000}`;
-      }
-
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw response;
-      }
-
-      const data = await response.json();
-      return data.value;
-    } catch (e) {
-      return null;
+    let url = `${QServerBaseUrl}/statistics/number-of-items`;
+    if (inLastDaysCount) {
+      url += `/${Date.now() - inLastDaysCount * 24 * 60 * 60 * 1000}`;
     }
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw response;
+    }
+
+    const data = await response.json();
+    return data.value;
   }
 }
